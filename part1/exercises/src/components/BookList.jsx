a3f849528c352e42482d881da8a73337121361c3
Kayalvizhi Rajendran <kayalvizhi.btech@gmail.com>
2024-08-26 17:59:52 -0500

    Excercise - React1 - part1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book List";
+   let book1 = "https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781501187742/fire-in-the-sky-9781501187742_lg.jpg";
+   let book2 = "https://www.booklistqueen.com/wp-content/uploads/everyone-here-is-lying-shari-lapena.jpg";
+   let book3 = "https://www.booklistqueen.com/wp-content/uploads/hello-stranger-katherine-center.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Fire in the Sky" />
+         <img src={book2} alt="Everyone Here is Lying" />
+         <img src={book3} alt="Hello Stranger" />
       </div>      
    );
 }
